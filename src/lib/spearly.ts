0c8a3907bc27cb51773a0885f160d008dd8824be
NakamaK <nakamura.makoto2020@gmail.com>
2024-01-08 05:03:47 +0900

    first commit

--- src/lib/spearly.ts
+++ src/lib/spearly.ts
@@ -2,12 +2,5 @@ import { SpearlyApiClient } from "@spearly/sdk-js";
 
 const API_DOMAIN = "api.spearly.com"
 
-export const spearlyApiClient = new SpearlyApiClient(import.meta.env.SPEARLY_API_KEY, API_DOMAIN)
-
-export interface BlogPost {
-    title: string;
-    date: Date;
-    description: string;
-    body: string;
-    alias: string;
-}
+export const spearlyApiClient = 
+  new SpearlyApiClient(import.meta.env.SPEARLY_API_KEY, API_DOMAIN)
